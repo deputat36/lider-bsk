8ca8599fe167718f97ed1ae1638547be694c16a0
deputat36 <a.v.kovtun@borisoglebsk.etagi.com>
2026-06-28 17:35:00 +0300

    Restore quote escaping in access admin UI

--- crm/v4/assets/v4/user-admin-v1.js
+++ crm/v4/assets/v4/user-admin-v1.js
@@ -27,7 +27,7 @@ function esc(value) {
     .replace(/&/g, '&amp;')
     .replace(/</g, '&lt;')
     .replace(/>/g, '&gt;')
-    .replace(/\"/g, '&quot;')
+    .replace(/"/g, '&quot;')
     .replace(/'/g, '&#039;');
 }
 
